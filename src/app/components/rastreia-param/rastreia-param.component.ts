77a5c438f53a64a32ea5cb66d89140f989d1d77e
AnnaLu <correioaninha@gmail.com>
2025-03-14 11:01:33 -0300

    organiza visualmente

--- src/app/components/rastreia-param/rastreia-param.component.ts
+++ src/app/components/rastreia-param/rastreia-param.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit, Inject, PLATFORM_ID, ViewChild, ElementRef } from '@angular/core';
+import { RouterModule, ActivatedRoute } from '@angular/router';
 import { PedidoService } from '../../pedido.service';
-import { ActivatedRoute } from '@angular/router';
 import { ApiService } from '../../api.service';
 import { isPlatformBrowser, CommonModule } from '@angular/common';
 
@@ -15,12 +14,22 @@ import { isPlatformBrowser, CommonModule } from '@angular/common';
 export class RastreiaParamComponent implements OnInit {
   pedido: string | null = null;
   mostrarModal: boolean = false;
-  imagemPromocao: string = 'assets/images/promocoes/default.jpg'; 
-  linkPromocao: string = 'https://www.fidcomex.com.br'; 
+  imagemPromocao: string = 'assets/images/promocoes/default.jpg';
+  linkPromocao: string = 'https://www.fidcomex.com.br';
 
+  @ViewChild('spinner', { static: false }) spinner!: ElementRef;
+  @ViewChild('cardPedidos', { static: false }) cardPedidos!: ElementRef;
+
+  private promocoesMap = [
+    { keywords: ['CARRINHO DE', 'TROCADOR'], img: 'assets/images/promocoes/bb_conforto.jpg', link: 'https://www.fidcomex.com.br/bebes/bebe-conforto/' },
+    { keywords: ['BEBÊ', 'ANDADOR', 'BEBE'], img: 'assets/images/promocoes/coisas_bb.jpg', link: 'https://www.fidcomex.com.br/bebes/' },
+    { keywords: ['COOKTOP', 'FOGÃO', 'GELADEIRA', 'FORNO', 'FRIGOBAR'], img: 'assets/images/promocoes/eletroportateis.jpg', link: 'https://www.fidcomex.com.br/eletroportateis/' },
+    { keywords: ['FREEZER', 'AIR FRYER', 'CLIMATIZADOR'], img: 'assets/images/promocoes/eletrodoemsticos_1.jpg', link: 'https://www.fidcomex.com.br/eletrodomestico/' },
+    { keywords: ['MALA', 'MOCHILA'], img: 'assets/images/promocoes/malas_mochilas.jpg', link: 'https://www.fidcomex.com.br/bolsas-malas-e-sacolas/' }
+  ];
 
   constructor(
-    private route: ActivatedRoute, 
+    private route: ActivatedRoute,
     private apiService: ApiService,
     private pedidoService: PedidoService,
     @Inject(PLATFORM_ID) private platformId: Object
@@ -41,47 +50,28 @@ export class RastreiaParamComponent implements OnInit {
   }
 
   verificaPromocao(response: any) {
-    if (response) {
-      const produto = response[0].produtos[0];  
-      console.log(produto)
-      if (produto.descricao_fiscal?.toUpperCase().includes('CARRINHO DE') ||produto.descricao_fiscal?.toUpperCase().includes('TROCADOR')) {  
-        this.imagemPromocao = 'assets/images/promocoes/bb_conforto.jpg';
-        this.linkPromocao = 'https://www.fidcomex.com.br/bebes/bebe-conforto/';
-      }else if(produto.descricao_fiscal?.toUpperCase().includes('BEBÊ') ||
-                produto.descricao_fiscal?.toUpperCase().includes('ANDADOR')  ||
-                produto.descricao_fiscal?.toUpperCase().includes('BEBE')){
-        this.imagemPromocao = 'assets/images/promocoes/coisas_bb.jpg';
-        this.linkPromocao = 'https://www.fidcomex.com.br/bebes/';
-      }else if(produto.descricao_fiscal?.toUpperCase().includes('COOKTOP') ||
-              produto.descricao_fiscal?.toUpperCase().includes('FOGÃO') ||
-              produto.descricao_fiscal?.toUpperCase().includes('GELADEIRA') ||
-              produto.descricao_fiscal?.toUpperCase().includes('FORNO') ||
-              produto.descricao_fiscal?.toUpperCase().includes('FRIGOBAR') ){
-        this.imagemPromocao = 'assets/images/promocoes/eletroportateis.jpg';
-        this.linkPromocao = 'https://www.fidcomex.com.br/eletroportateis/';
-      }else if(produto.descricao_fiscal?.toUpperCase().includes('FREEZER') ||
-              produto.descricao_fiscal?.toUpperCase().includes('AIR FRYER')||
-              produto.descricao_fiscal?.toUpperCase().includes('CLIMATIZADOR')){
-        this.imagemPromocao = 'assets/images/promocoes/eletrodoemsticos_1.jpg';
-        this.linkPromocao = 'https://www.fidcomex.com.br/eletrodomestico/';
-      }else if(produto.descricao_fiscal?.toUpperCase().includes('MALA') ||
-              produto.descricao_fiscal?.toUpperCase().includes('MOCHILA')){
-        this.imagemPromocao = 'assets/images/promocoes/malas_mochilas.jpg';
-        this.linkPromocao = 'https://www.fidcomex.com.br/bolsas-malas-e-sacolas/';
-        }
-    } else {
+    if (!response || !response[0]?.produtos?.length) {
       console.warn('Objeto response não contém produtos válidos:', response);
+      return;
+    }
+
+    const descricao = response[0].produtos[0].descricao_fiscal?.toUpperCase() || '';
+
+    const promocao = this.promocoesMap.find(promo => promo.keywords.some(kw => descricao.includes(kw)));
+    if (promocao) {
+      this.imagemPromocao = promocao.img;
+      this.linkPromocao = promocao.link;
     }
   }
 
-  buscarPedido(pedido: string) {
+  async buscarPedido(pedido: string) {
     if (isPlatformBrowser(this.platformId)) {
       const spinner = document.getElementById('spinner');
       const card = document.getElementById('card-pedidos');
-
+  
       spinner?.classList.remove('d-none');
       card?.classList.add('d-none');
-
+  
       this.apiService.enviarCpfCnpj(pedido).subscribe({
         next: (response) => {
           this.pedidoService.atualizarPedidos(response);
@@ -96,6 +86,13 @@ export class RastreiaParamComponent implements OnInit {
           card?.classList.remove('d-none');
         }
       });
+    }
+  }
+
+  private toggleLoading(isLoading: boolean) {
+    if (this.spinner && this.cardPedidos) {
+      this.spinner.nativeElement.classList.toggle('d-none', !isLoading);
+      this.cardPedidos.nativeElement.classList.toggle('d-none', isLoading);
     }
   }
 }
